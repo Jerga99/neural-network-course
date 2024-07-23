a11feb14fabbab5cde8a9fa251f631f6ab5e9540
Filip Jerga <jerga99@gmail.com>
2024-07-23 10:44:49 +0200

    Define data in code

---
+++ perceptron.js
@@ -0,0 +1,17 @@
+
+
+
+
+const trainInputs = [
+  [2, 7],
+  [3, 6],
+  [1, 1],
+  [1, 2],
+  [2, 1],
+];
+
+const trainLabels = [1, 1, 0, 0, 0];
+
+const weights = [0.1, 0.3];
+const b = 0.5; // bias
+const n = 0.1; // learning rate
