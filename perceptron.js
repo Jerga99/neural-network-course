7f62dd3aff8af023eea03e07b2e724b46c773e5a
Filip Jerga <jerga99@gmail.com>
2024-07-24 08:37:24 +0200

    Compute sums in code

--- perceptron.js
+++ perceptron.js
@@ -12,6 +12,27 @@ const trainInputs = [
 
 const trainLabels = [1, 1, 0, 0, 0];
 
-const weights = [0.1, -0.3];
-const b = 0.5; // bias
-const n = 0.1; // learning rate
+class Perceptron {
+  constructor(learningRate = 0.1) {
+    this.weights = [0.1, -0.3];
+    this.bias = 0.5;
+    this.learningRate = learningRate;
+  }
+
+  train(trainData) {
+    for (let i = 0; i < trainData.length; i++) {
+      let sum = this.bias;
+      let inputs = trainData[i];
+
+      for (let j = 0; j < inputs.length; j++) {
+        sum += inputs[j] * this.weights[j];
+      }
+
+      console.log(sum);
+    }
+  }
+
+}
+
+const perceptron = new Perceptron();
+perceptron.train(trainInputs);
