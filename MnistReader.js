d6c86b9f630d1a4a27e85ecb2e8c6bb602440369
Filip Jerga <jerga99@gmail.com>
2024-07-24 13:13:35 +0200

    Read info bytes

--- MnistReader.js
+++ MnistReader.js
@@ -5,27 +5,20 @@ function readIdxFile(filepath) {
   const data = fs.readFileSync(filepath);
   let offset = 0;
   const magicNumber = data.readUint32BE(offset);
-  // 0 0 8 3
-  // 8 -> uint8 -> 8 bits -> 1 byte -> 0 - 255
-  // 3 -> 4 bytes(number of items) - 4 bytes(width - rows) - 4 bytes(height - cols)
-  console.log(magicNumber);
 
+  offset += 4;
+  const numberOfItems = data.readUint32BE(offset);
+  offset += 4;
+  const rows = data.readUint32BE(offset);
+  offset += 4;
+  const cols = data.readUint32BE(offset);
 }
 
 readIdxFile("./datasets/mnist/train-images.idx3-ubyte");
 
 
-
-
-
-
-// 11101010 - binary
-// EA - hex
-// 234 - decimal
-
-
-// BE -> 0 0 8 3 , LE -> 3 8 0 0
-
-// 00001000 -> 8
-// 00000011 -> 3
-// 0000100000000011
+// 0 0 0 4 0 0 0 4 0 100 255 27 0 255 255 27 0 0 255 27 0 0 255 0
+// 0 100 255 27
+// 0 255 255 27
+// 0  0  255 27
+// 0  0  255 0
