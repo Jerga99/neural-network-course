93e9511b52f1441a28ff01edc20f9541fdd90135
Filip Jerga <jerga99@gmail.com>
2024-07-25 08:26:36 +0200

    Show label file

--- MnistReader.js
+++ MnistReader.js
@@ -5,20 +5,22 @@ function readIdxFile(filepath) {
   const data = fs.readFileSync(filepath);
   let offset = 0;
   const magicNumber = data.readUint32BE(offset);
-
   offset += 4;
   const numberOfItems = data.readUint32BE(offset);
   offset += 4;
-  const rows = data.readUint32BE(offset);
-  offset += 4;
-  const cols = data.readUint32BE(offset);
+
+  // label file
+  if (magicNumber === 2049) {
+    console.log("Label file!");
+  } else {
+    // image file
+
+    const rows = data.readUint32BE(offset);
+    offset += 4;
+    const cols = data.readUint32BE(offset);
+  }
 }
 
 readIdxFile("./datasets/mnist/train-images.idx3-ubyte");
 
-
-// 0 0 0 4 0 0 0 4 0 100 255 27 0 255 255 27 0 0 255 27 0 0 255 0
-// 0 100 255 27
-// 0 255 255 27
-// 0  0  255 27
-// 0  0  255 0
+// readIdxFile("./datasets/mnist/train-labels.idx1-ubyte");
