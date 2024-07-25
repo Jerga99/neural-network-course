35f9a0a56178a2c452c35d0bfbf6cc14840ce65d
Filip Jerga <jerga99@gmail.com>
2024-07-25 09:21:13 +0200

    Init home and navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,36 @@
+import Home from "./pages/Home";
+
+function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <div className="link" onClick={() => alert("Go Home!")}>
+            Home
+          </div>
+        </li>
+        <li>
+          <div className="link" onClick={() => alert("Preview test images")}>
+            Preview test images
+          </div>
+        </li>
+        <li>
+          <div className="link" onClick={() => alert("Test Perceptron")}>
+            Test Perceptron
+          </div>
+        </li>
+      </ul>
+    </nav>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <div className="app-container">
+        <Navigation />
+        <Home />
+      </div>
     </div>
   );
 }
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,16 @@
+
+
+
+function Home() {
+  return (
+    <>
+      <div className="page-container">
+        <div className="page-header">
+          Home Page
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Home;
