ceca0938be39c6c4458af9ce16b9a023ed3e1696
Filip Jerga <jerga99@gmail.com>
2024-07-26 08:22:02 +0200

    Finish routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Home from "./pages/Home";
 import PreviewTestImages from "./pages/mnist/PreviewTestImages";
 
 function Navigation() {
+
+  const pushState = (path) => (e) => {
+    e.preventDefault();
+    window.history.pushState({}, "", path);
+    window.dispatchEvent(new PopStateEvent("popstate"));
+  }
+
   return (
     <nav>
       <ul>
         <li>
-          <div className="link" onClick={() => alert("Go Home!")}>
+          <div className="link" onClick={pushState("/")}>
             Home
           </div>
         </li>
         <li>
-          <div className="link" onClick={() => alert("Preview test images")}>
+          <div className="link" onClick={pushState("/mnist/test-images")}>
             Preview test images
           </div>
         </li>
         <li>
-          <div className="link" onClick={() => alert("Test Perceptron")}>
+          <div className="link" onClick={pushState("/mnist/test-perceptron")}>
             Test Perceptron
           </div>
         </li>
@@ -28,6 +35,18 @@ function Navigation() {
 
 function Router() {
   const [pathname, setPathname] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const handlePopState = () => {
+      setPathname(window.location.pathname);
+    }
+
+    window.addEventListener("popstate", handlePopState);
+
+    return () => {
+      window.removeEventListener("popstate", handlePopState);
+    };
+  }, []);
 
   switch(pathname) {
     case "/mnist/test-images":
