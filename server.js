ac532d0c128382995a02a2fe38719d59da7b69fc
Filip Jerga <jerga99@gmail.com>
2024-07-30 10:09:13 +0200

    Store misclassified data

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
+const fs = require("fs");
 const PORT = 3001;
 
 app.use(cors());
@@ -9,11 +11,38 @@ app.use(express.json());
 app.post("/save-misclassified", (req, res) => {
   const { input, label } = req.body;
 
-  console.log(input);
-  console.log(label);
+  const filePath = path.join(__dirname, "datasets", "mnist", "misclassified-data.json");
 
+  fs.readFile(filePath, (err, data) => {
+    if (err && err.code === "ENOENT") {
+      const data = {labels: [label], inputs: [input]};
+      const stringData = JSON.stringify(data, null, 0);
 
-  res.status(200).send("Hello World");
+      fs.writeFile(filePath, stringData, (err) => {
+        if (err) {
+          res.status(500).send("Error saving data!");
+        } else {
+          res.status(200).send("Data saved succesfuly!");
+        }
+      });
+    } else if (err) {
+      res.status(500).send("Error reading file");
+    } else {
+      const jsonData = JSON.parse(data);
+
+      jsonData.labels.push(label);
+      jsonData.inputs.push(input);
+
+      fs.writeFile(filePath, JSON.stringify(jsonData, null, 0), (err) => {
+        if (err) {
+          res.status(500).send("Error saving file!");
+        } else {
+          res.status(200).send("Data saved succesfuly!");
+        }
+      });
+    }
+  });
+
 });
 
 app.listen(PORT, () => {
