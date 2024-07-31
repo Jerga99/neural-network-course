6b5ea1181545baddc11cec50f5ce7bf93373f06f
Filip Jerga <jerga99@gmail.com>
2024-07-31 09:46:49 +0200

    Compute hidden sum in code

--- mlp.js
+++ mlp.js
@@ -17,7 +17,17 @@ class MLP {
 
     this.biasesOutput = [0.1, 0.1];
   }
+
+  forward(inputs) {
+    const hiddenSum = this.weightsInputHidden.map((weights, i) => {
+      return weights.reduce((sum, weight, j) => sum + (weight * inputs[j]), this.biasesHidden[i]);
+    });
+
+    console.log(hiddenSum);
+  }
 }
 
 const mlp = new MLP();
-const input = [0.1, 0.2, 0.3, 0.4];
+const image = [0.1, 0.2, 0.3, 0.4];
+
+mlp.forward(image);
