6c0ed7d551e5407bf259893f1dff05187956e907
Filip Jerga <jerga99@gmail.com>
2024-08-02 08:26:53 +0200

    Compute output probabilities

--- mlp.js
+++ mlp.js
@@ -22,6 +22,21 @@ class MLP {
     return Math.max(0, z);
   }
 
+  // softmax(outputs) {
+  //   return outputs.map((output, i) => {
+  //     const nominator = Math.exp(output);
+  //     const denominator = outputs.reduce((sum, val) => sum + Math.exp(val), 0);
+  //     return nominator / denominator;
+  //   });
+  // }
+
+  softmax(outputs) {
+    const maxOutput = Math.max(...outputs);
+    const expValues = outputs.map(output => Math.exp(output - maxOutput));
+    const sumExpValues = expValues.reduce((sum, val) => sum + val, 0);
+    return expValues.map(val => val / sumExpValues);
+  }
+
   forward(inputs) {
     const hiddenSums = this.weightsInputHidden.map((weights, i) => {
       return weights.reduce((sum, weight, j) => sum + (weight * inputs[j]), this.biasesHidden[i]);
@@ -33,11 +48,12 @@ class MLP {
       return weights.reduce((sum, weight, j) => sum + (weight * hiddenActivations[j]), this.biasesOutput[i])
     });
 
-    console.log(outputSums);
+    return this.softmax(outputSums);
   }
 }
 
 const mlp = new MLP();
 const image = [0.1, 0.2, 0.3, 0.4];
 
-mlp.forward(image);
+const outputProbabilities = mlp.forward(image);
+console.log(outputProbabilities)
